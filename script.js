62ebb038e572798d98542e388c9c9c066cd9d433
Serenity Le Underwood <le.serenityu@gmail.com>
2023-06-28 12:14:27 -0500

    Add files and basic structure

---
+++ script.js
@@ -0,0 +1,8 @@
+// create a container div; 960px wide (constant)
+
+// create the grid divs inside container div
+
+// hover effect of individual divs
+
+// button div; asks user for number of squares/side
+// set max limit to 100
